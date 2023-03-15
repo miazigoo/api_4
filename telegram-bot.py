3dfcc8d3e8358c81a64d8e257864cf1064404baa
Bloodov Pavel <kasper6024@gmail.com>
2023-03-15 13:00:12 +0300

    publish photo to Telegram channel.

--- telegram-bot.py
+++ telegram-bot.py
@@ -3,12 +3,12 @@ import os
 import telegram
 from dotenv import load_dotenv
 
-
 load_dotenv()
 token = os.getenv('TG_TOKEN')
 channel = os.getenv('TG_CHANNEL_LOGIN')
 bot = telegram.Bot(token=token)
 
+img = 'images/picture_of_the_day/Soul_Jimenez_1080.jpg'
 
-bot.send_message(chat_id=channel, text="В канале ZZzz.")
+bot.send_photo(chat_id=channel, photo=open(img, 'rb'))
 print(bot.get_me())
