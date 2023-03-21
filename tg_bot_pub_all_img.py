990782a05ba11917f197ebf24478bbfa5153d65f
Bloodov Pavel <kasper6024@gmail.com>
2023-03-21 13:26:33 +0300

    Убраны лишние условия

--- tg_bot_pub_all_img.py
+++ tg_bot_pub_all_img.py
@@ -6,7 +6,7 @@ import telegram
 from dotenv import load_dotenv
 
 
-def get_command_line_argument():
+def get_command_line_argument(env_publish_time):
     """parse arg"""
     parser = argparse.ArgumentParser(
         description="""
@@ -14,7 +14,7 @@ def get_command_line_argument():
         Если не указан при запуске аргумент, публикация будет проходить раз в 4 часа.
         Аргумент принимает целые и дробные числа. Указанное число - это время в часах. 
         """)
-    parser.add_argument('hour', nargs='?', help='Укажите периодичность публикации: ', type=float)
+    parser.add_argument('hour', nargs='?', help='Укажите периодичность публикации: ', type=float, default=env_publish_time)
     hour = parser.parse_args().hour
 
     return hour
@@ -27,10 +27,10 @@ def main():
     channel = os.environ['TG_CHANNEL_LOGIN']
     bot = telegram.Bot(token=token)
     sec_in_hour = 3600
-    command_line_argument = get_command_line_argument()
-    tg_publish_time = float(os.getenv('TG_PUBLISH_TIME', default=4)) * sec_in_hour
-    if command_line_argument:
-        tg_publish_time = command_line_argument * sec_in_hour
+
+    env_publish_time = float(os.getenv('TG_PUBLISH_TIME', default=4))
+    command_line_argument = get_command_line_argument(env_publish_time)
+    tg_publish_time = command_line_argument * sec_in_hour
 
     while True:
         for img in img_files:
--- tg_bot_publish_img.py
+++ tg_bot_publish_img.py
@@ -5,14 +5,14 @@ import argparse
 from dotenv import load_dotenv
 
 
-def get_command_line_argument():
+def get_command_line_argument(img_random):
     """parse arg"""
     parser = argparse.ArgumentParser(
         description="""
         Программа публикует указанную фотографию из директории images в Telegram канал.
         Если фотография не указана, опубликуется случайная фотография из директории images. 
         """)
-    parser.add_argument('img', nargs='?', help='Укажите файл в директории images в формате img.png : ')
+    parser.add_argument('img', nargs='?', help='Укажите файл в директории images в формате img.png : ', default=img_random)
     img = parser.parse_args().img
 
     return img
@@ -21,13 +21,11 @@ def get_command_line_argument():
 def main():
     load_dotenv()
     img_files = os.listdir('images')
-    img_name = random.choice(img_files)
+    img_random = random.choice(img_files)
     token = os.environ['TG_TOKEN']
     channel = os.environ['TG_CHANNEL_LOGIN']
     bot = telegram.Bot(token=token)
-    command_line_argument = get_command_line_argument()
-    if command_line_argument:
-        img_name = command_line_argument
+    img_name = get_command_line_argument(img_random)
 
     with open(f'images/{img_name}', 'rb') as pulish_img:
         bot.send_photo(chat_id=channel, photo=pulish_img)
